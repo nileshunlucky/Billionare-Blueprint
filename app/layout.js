91fb2bc26878fb9b896603e0d10bd4ac7c7e90e0
unluckynilesh <nileshinde001@gmail.com>
2025-03-10 12:43:03 +0530

    meta pixel

--- app/layout.js
+++ app/layout.js
@@ -13,17 +13,40 @@ const geistMono = Geist_Mono({
 
 export const metadata = {
   title: "Billionaire Blueprint",
-  description: "Top 10 Most valueable Business & Finace ebook bundle",
+  description: "Top 10 Most Valuable Business & Finance eBook Bundle",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head >
+      <head>
+        {/* Meta Pixel */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              n.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(n,s)}(window,document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1000661781942365');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1000661781942365&ev=PageView&noscript=1"
+          />
+        </noscript>
       </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
       </body>
     </html>
--- app/thank-you/page.js
+++ app/thank-you/page.js
@@ -1,8 +1,13 @@
-import React from "react";
+"use client";
+
+import React, { useEffect } from "react";
 import Link from "next/link";
 // import { FaCheckCircle } from "react-icons/fa"; 
 
 const ThankYouPage = () => {
+  useEffect(() => {
+    window.fbq('track', 'Purchase', { value: 299.99, currency: 'INR' });
+  }, []);
   return (
     <div className="flex justify-center items-center h-screen bg-gradient-to-br from-blue-600 to-blue-900 p-3">
       <div className="bg-white p-8 rounded-xl shadow-xl text-center max-w-md w-full">
